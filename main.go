d7ebfbcd48081a503091e8ac11841d137958981d
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-11-21 10:57:56 +0900

    Add listFiles

--- main.go
+++ main.go
@@ -10,18 +10,30 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "pptgrep"
-	app.Usage = "Searching PPTfile which has input words"
+	app.Usage = "Searching PPTfile which has an input word"
 	app.Version = "1.0.0"
-
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "recursive, r",
-			Usage: "Searchin recursively",
+			Name:  "word, w",
+			Usage: "Searching files which has the word",
+		},
+		cli.StringFlag{
+			Name:  "path, p",
+			Value: ".",
+			Usage: "Starting point",
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello")
+		if c.String("word") != "" {
+			files := listFiles(c.String("path"))
+			search(files, c.String("word"))
+			for _, file := range files {
+				fmt.Println(file)
+			}
+		} else {
+
+		}
 		return nil
 	}
 
---
+++ search.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+func listFiles(searchPath string) []string {
+	var files = []string{}
+	searchPath, err := filepath.Abs(searchPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fis, err := ioutil.ReadDir(searchPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fis {
+		fullPath := filepath.Join(searchPath, fi.Name())
+		if !fi.IsDir() {
+			files = append(files, fullPath)
+		}
+	}
+	return files
+}
+
+func search(files []string, searchPath string) {
+
+}

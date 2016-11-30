33fe34e38c1bbf317a4870b9b7ed60526ef0c3c4
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-11-30 14:11:16 +0900

    Add recursive

--- main.go
+++ main.go
@@ -8,32 +8,54 @@ import (
 )
 
 func main() {
+
+	var word string
+	var path string
+	var recursive bool
+
 	app := cli.NewApp()
 	app.Name = "pptgrep"
 	app.Usage = "Searching PPTfile which has an input word"
 	app.Version = "1.0.0"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "word, w",
-			Usage: "Searching files which has the word",
+			Name:        "word, w",
+			Usage:       "Searching files which has the word",
+			Destination: &word,
 		},
 		cli.StringFlag{
-			Name:  "path, p",
-			Value: ".",
-			Usage: "Starting point",
+			Name:        "path, p",
+			Value:       ".",
+			Usage:       "Starting point",
+			Destination: &path,
+		},
+		cli.BoolFlag{
+			Name:        "r",
+			Usage:       "Searching Recursively",
+			Destination: &recursive,
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		if c.String("word") != "" {
-			files := listFiles(c.String("path"))
+		if word != "" {
+
+			// collect files
+			var files []string
+			if !recursive {
+				files = listFiles(path)
+			} else {
+				files = listFilesWalk(path)
+			}
+
+			// search file which has the word
 			for _, file := range files {
 				if isPpt(file) {
-					if containWord(file, c.String("word")) {
+					if containWord(file, word) {
 						fmt.Println(file)
 					}
 				}
 			}
+
 		} else {
 			fmt.Println(`Please look at usage. ( $ ./pptgrep -h )`)
 		}

a4d4b943f7fa0fea876d2bd3708a7dc20776fd85
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-11-30 14:11:16 +0900

    Add recursive

--- list.go
+++ list.go
@@ -24,3 +24,25 @@ func listFiles(searchPath string) []string {
 	}
 	return files
 }
+
+func listFilesWalk(searchPath string) []string {
+	var files = []string{}
+	searchPath, err := filepath.Abs(searchPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fis, err := ioutil.ReadDir(searchPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fi := range fis {
+		fullPath := filepath.Join(searchPath, fi.Name())
+		if !fi.IsDir() {
+			files = append(files, fullPath)
+		} else {
+			files = append(files, listFilesWalk(fullPath)...)
+		}
+	}
+	return files
+}
